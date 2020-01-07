c1f1579bfd7cd49cad70a2ad715fb28a98b4482c
cs896afk <52067790+cs896afk@users.noreply.github.com>
2020-01-07 21:59:38 +0100

    Update pythonexercise10.py

--- pythonexercise10.py
+++ pythonexercise10.py
@@ -1,1 +1,8 @@
 #!/bin/python3
+
+import random
+a = random.sample(range(1, 50), random.randint(10, 40))
+b = random.sample(range(1, 50), random.randint(10, 40))
+
+print(len(a), a, "\n", len(b), b, "\n")
+print([value for value in a if (value in b)])
